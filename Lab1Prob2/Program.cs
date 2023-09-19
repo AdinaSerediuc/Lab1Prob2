5feb130d7af065da48d28ef0a115b0d086d5883c
Ada Ababei <contact@adina_ababei@yahoo.com>
2023-09-19 17:28:18 +0300

    Problema2

--- Lab1Prob2/Program.cs
+++ Lab1Prob2/Program.cs
@@ -8,6 +8,20 @@ namespace Lab1Prob2
     {
         static void Main(string[] args)
         {
+            /*Ex2
+Scrieti un program care va calcula media aritmetica a trei numere citite de la
+tastatura
+            */
+
+            int numar1 = int.Parse(Console.ReadLine());
+            int numar2 = int.Parse(Console.ReadLine());
+            int numar3 = int.Parse(Console.ReadLine());
+
+            int suma = numar1 + numar2 + numar3;
+            int medieAritmetica = suma / 3;
+
+            Console.WriteLine("Media aritmetica a celor 3 numere este =" + medieAritmetica);
+
         }
     }
 }
